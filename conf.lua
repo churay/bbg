0e299d806048832b2718877d1a382538b0a05679
Joseph Ciurej <churayy@gmail.com>
2015-04-25 09:09:42 -0700

    Updated the makefile name to be more consistent with the naming scheme for the rest of the project. Modified the game configuration file to have fewer comments.

--- conf.lua
+++ conf.lua
@@ -1,18 +1,15 @@
--- The configuration loading function for the application.  This function is
--- run before any other functions to bootstrap the application and initialize
--- important variables.
 function love.conf( config )
-  config.window.title = "bbg"     -- Window Title
-  config.window.width = 640       -- Screen Width
-  config.window.height = 480      -- Screen Height
-  config.window.vsync = true      -- Vertical Sync
+  config.window.title = "bbg"
+  config.window.width = 640
+  config.window.height = 480
+  config.window.vsync = true
 
-  config.modules.audio = false    -- Audio Module
-  config.modules.event = true     -- Event Module
-  config.modules.graphics = true  -- Graphics Module
-  config.modules.physics = false  -- Physics Module
-  config.modules.system = true    -- System Module
-  config.modules.timer = true     -- Timer Module
-  config.modules.window = true    -- Window Module
-  config.modules.thread = true    -- Thread Module
+  config.modules.audio = false
+  config.modules.event = true
+  config.modules.graphics = true
+  config.modules.physics = false
+  config.modules.system = true
+  config.modules.timer = true
+  config.modules.window = true
+  config.modules.thread = true
 end
