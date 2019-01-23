2a8d5e3ccc83dde5402fb654e0efde2e0ba59083
Joseph Ciurej <churayy@gmail.com>
2019-01-23 23:06:07 +0000

    * Revised the 'bubble_t' type to use enhanced type conventions.

--- bbg/bubble_t.lua
+++ bbg/bubble_t.lua
@@ -1,61 +1,75 @@
-local Class = require( 'Class' )
-local Vector = require( 'Vector' )
-local Box = require( 'Box' )
-local Color = require( 'Color' )
-local Bubble = Class()
+local struct = require( 'bbg.struct' )
+local util = require( 'util' )
+local color = require( 'bbg.color' )
+local bbox_t = require( 'bbg.bbox_t' )
+local vector_t = require( 'bbg.vector_t' )
 
-Bubble.COLORS = { Color.byname('red'), Color.byname('green'), Color.byname('blue'), Color.byname('yellow') }
+local BUBBLECOLORS = { 'red', 'green', 'blue', 'yellow' }
+for bubbleidx = 1, #BUBBLECOLORS, 1 do
+  BUBBLECOLORS[bubbleidx] = color.byname(BUBBLECOLORS[bubbleidx])
+end
+
+local DEBUGCOLOR = { 0.9, 1.0, 0.0 }
 
 --[[ Constructors ]]--
 
-function Bubble._init( self, pos, vel, colval )
-  local pos = pos or Vector( 0.0, 0.0 )
-  local vel = vel or Vector( 0.0, 0.0 )
-  local colval = colval or math.random( #Bubble.COLORS )
+local bubble_t = struct( {},
+  '_pos', vector_t(),
+  '_vel', vector_t(),
+  '_color', color.byname('black'),
+)
 
-  self._pos = pos
-  self._vel = vel
-  self._color = Bubble.COLORS[colval]
+function bubble_t._init( self, ... )
+  local args = { ... }
+
+  if #args >= 3 then
+    self._pos.x, self._pos.y = args[1].x, args[1].y
+    self._vel.x, self._vel.y = args[2].x, args[2].y
+  end
+
+  local colval = args[#args] or math.random( #BUBBLECOLORS )
+  self._color = BUBBLECOLORS[colval]
 end
 
 --[[ Public Functions ]]--
 
-function Bubble.update( self, dt )
+function bubble_t.update( self, dt )
   self._pos = self._pos + dt * self._vel
 end
 
-function Bubble.draw( self, canvas )
+function bubble_t.draw( self, canvas )
   canvas.push()
   canvas.translate( self._pos:getxy() )
 
-  canvas.setColor( unpack(self._color) )
+  canvas.setColor( util.unpack(self._color) )
   canvas.circle( 'fill', 0.0, 0.0, 0.5, 20.0 )
 
-  -- TODO(JRC): Remove this functionality after debugging is complete.
-  canvas.setColor( Color.from255(234, 255, 0) )
+  -- TODO(JRC): Only enable this functionality when the debug flag
+  -- is enabled.
+  canvas.setColor( util.unpack(DEBUGCOLOR) )
   canvas.rectangle( 'line', -0.5, -0.5, 1.0, 1.0 )
   canvas.pop()
 end
 
-function Bubble.bounce( self )
-  self._vel = self._vel + Vector( -2.0 * self._vel:getx(), 0.0 )
+function bubble_t.bounce( self )
+  self._vel = self._vel + vector_t( -2.0 * self._vel:getx(), 0.0 )
 end
 
-function Bubble.stop( self )
-  self._vel = Vector( 0.0, 0.0 )
+function bubble_t.stop( self )
+  self._vel = vector_t( 0.0, 0.0 )
 end
 
-function Bubble.getbbox( self )
+function bubble_t.getbbox( self )
   local minx = self._pos:getx() - 0.5
   local miny = self._pos:gety() - 0.5
 
-  return Box( minx, miny, 1.0, 1.0 )
+  return bbox_t( minx, miny, 1.0, 1.0 )
 end
 
 --[[ Accessor Functions ]]--
 
-function Bubble.getcenter( self ) return self._pos end
-function Bubble.getvelocity( self ) return self._vel end
-function Bubble.getcolor( self ) return self._color end
+function bubble_t.getcenter( self ) return self._pos end
+function bubble_t.getvelocity( self ) return self._vel end
+function bubble_t.getcolor( self ) return self._color end
 
-return Bubble
+return bubble_t
